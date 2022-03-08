ca33a165e63dd9bfa748f0014b6c7d4504c36229
Y H Falls <55866075+yfalls@users.noreply.github.com>
2022-03-08 11:15:29 -0800

    tf

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from keras.models import load_model
+#from keras.models import load_model
+from tensorflow.keras.models import load_model
 from keras.preprocessing import image
 import os
 import numpy as np
